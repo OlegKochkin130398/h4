3cd4d8717cfcfc2bc70b479a9581ea267808a9cd
oleg- <o.boys@yandex,ru>
2022-08-19 16:42:19 +0300

    4 Задание

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int deliveryDistance = 20;
+        int Day;
+        if(deliveryDistance <= 20){
+            Day = 1;
+            System.out.println("Потребуется дней: "+Day);
+        }
+        if(deliveryDistance > 20 && deliveryDistance == 60){
+            Day = 2;
+            System.out.println("Потребуется дней: "+Day);
+        }
+        if(deliveryDistance > 60 && deliveryDistance == 100){
+            Day = 3;
+            System.out.println("Потребуется дней: "+Day);
+        }
+    }
 }
